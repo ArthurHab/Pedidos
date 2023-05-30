b0989dd102c69a0594293a4ae9548b67f344dfbe
ArthurHab <arthur200106@gmail.com>
2023-05-30 13:21:52 -0300

    update ButtonProduct

--- src/app/components/buttonProduct.tsx
+++ src/app/components/buttonProduct.tsx
@@ -1,10 +1,10 @@
 import styles from '../styles/buttonProduct.module.css'
 
-export const ButtonProduct = (props: any) => {
+export const ButtonProduct = ({event, color, icon}: any) => {
     return(
         <>
-            <button className={styles.button} onClick={props.event} style={{background: props.color}}>
-                {props.icon}
+            <button className={styles.button} onClick={event} style={{background: color}}>
+                {icon}
             </button>
         </>
     )
